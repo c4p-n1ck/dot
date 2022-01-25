32bd7c5dfcf02f2fe9d169d3ca16e55d2a4e06ea
Captain Nick Lucifer* <nicksb4b3@gmail.com>
2022-01-25 20:07:44 +0545

    misc changes

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@
 
 mkdir -p $HOME/.config/nvim >/dev/null
 cp ./init.vim ~/.config/nvim
+
+echo "[+] Installing pip"
+curl https://bootstrap.pypa.io/get-pip.py -vs | python3
+
+pip install virtualenv
