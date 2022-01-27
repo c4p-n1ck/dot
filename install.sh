fe7a04176a5984b0b84b9cee8713ee358b69f629
Captain Nick Lucifer* <nicksb4b3@gmail.com>
2022-01-27 17:51:30 +0545

    misc changes

--- install.sh
+++ install.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
 
+if ! command -v pip >/dev/null; then
+	echo "[+] Installing pip"
+	curl https://bootstrap.pypa.io/get-pip.py -vs | python3
+fi
+pip install virtualenv
+
+
+if ! command -v nvim >/dev/null; then
+	echo "[+] Installing neovim"
+	wget $(curl -s https://api.github.com/repos/neovim/neovim/releases | grep stable | grep appimage | cut -d '"' -f 4 | head -n 1)
+	chmod +x ./nvim.appimage && sudo mv ./nvim.appimage /bin/neovim
+	sudo ln -s /bin/neovim /bin/nvim
+	sudo ln -s /bin/neovim /bin/vim
+	sudo mv /usr/bin/vi /usr/bin/vi.old
+	sudo ln -s /bin/nvim /bin/vi
+fi
+
+
+echo "[+] Installing Plug-ins for nvim"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p $HOME/.config/nvim >/dev/null
 cp ./init.vim ~/.config/nvim
-
-echo "[+] Installing pip"
-curl https://bootstrap.pypa.io/get-pip.py -vs | python3
-
-pip install virtualenv
+nvim --headless +PlugInstall +qa 2>/dev/null
