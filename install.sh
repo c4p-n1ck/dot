c2814998b6bbac53efcfba4a3616775153ebe5b5
Captain Nick Lucifer* <nicksb4b3@gmail.com>
2022-01-27 18:32:19 +0545

    misc changes

--- install.sh
+++ install.sh
@@ -21,4 +21,4 @@ echo "[+] Installing Plug-ins for nvim"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p $HOME/.config/nvim >/dev/null
 cp ./init.vim ~/.config/nvim
-nvim --headless +PlugInstall +qa 2>/dev/null
+nvim --headless +PlugInstall +qa >/dev/null & disown
