989f4ae8287030348615a84454be4a07ad0e5b4f
Captain Nick Lucifer* <naryal2580@gmail.com>
2022-02-02 06:07:48 +0545

    misc changes

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # dot
+My current [dot] files.
+
+**NOTE**: Make sure you've got a few stuff before you're expecting things to work out of the box
+
+##### Stuff you'll need
+
+- [neofetch](https://github.com/dylanaraps/neofetch)
+- [antibody](https://getantibody.github.io/)
+- [lsd](https://github.com/Peltoche/lsd)
+- [chroma](https://github.com/alecthomas/chroma) / pygmentize
+- [neovim](https://neovim.io/)
+- [xclip](https://github.com/astrand/xclip)
+- [nerdfonts*](https://www.nerdfonts.com/)
+
+a few stuff are handled by `install.sh`, some of them include: neofetch, antibody, lsd, chroma (you might need to install it manually), neovim, xclip. While you do need to install the any patched Nerd Font manually.
+
+**NOTE**: If some shell commands like `cat`, `less`, etc. are bugging you please add a backslash `\` before the command. Making it look like `\cat > lulz << EOF`
+
+---
+
+*You may also use any other **supported** font(s) too :slightly_smiling_face:
--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 if ! command -v pip >/dev/null; then
 	echo "[+] Installing pip"
 	curl https://bootstrap.pypa.io/get-pip.py -vs | python3
@@ -8,17 +7,35 @@ fi
 pip install virtualenv
 
 
-if ! command -v nvim >/dev/null; then
-	echo "[+] Installing neovim"
-	wget $(curl -s https://api.github.com/repos/neovim/neovim/releases | grep stable | grep appimage | cut -d '"' -f 4 | head -n 1)
-	chmod +x ./nvim.appimage && sudo mv ./nvim.appimage /bin/neovim
-	sudo ln -s /bin/neovim /bin/nvim
-	sudo ln -s /bin/neovim /bin/vim
-fi
+echo "[+] Installing via pacman."
+sudo pacman -Sy --needed lsd zsh neovim xclip neofetch
 
 
 echo "[+] Installing Plug-ins for nvim"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p $HOME/.config/nvim >/dev/null
-cp ./init.vim ~/.config/nvim
+cp ./config/nvim/init.vim "$HOME/.config/nvim"
 nvim --headless +PlugInstall +qa >/dev/null & disown
+clear
+
+
+echo "[+] Installing antibody"
+curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
+
+
+echo "[+] Configuring zsh"
+mkdir -p "$HOME/.config/zsh"
+
+mv "$HOME/.zshrc" "$HOME/.config/zsh/.zshrc.old"
+cp ./config/zsh/* $HOME/.config/zsh/
+
+printf 'source "$HOME/.config/zsh/zshrc"\ntypeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet\n' > "$HOME/.zshrc"
+mkdir chroma
+cd chroma
+chroma_url=$(curl -s https://api.github.com/repos/alecthomas/chroma/releases | grep linux-amd64 | head -n 2 | tail -n 1 | cut -d '"' -f 4)
+if [ ! $chroma_url ]; then echo "[-] Please install chroma from: https://github.com/alecthomas/chroma/releases" && xdg-open https://github.com/alecthomas/chroma/releases && exit; fi
+wget $chroma_url
+tar xvf chroma* >/dev/null
+sudo mv ./chroma /usr/bin/chroma
+rm COPYING README.md chroma*
+cd .. && rm -rf chroma
